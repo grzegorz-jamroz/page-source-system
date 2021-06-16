7976bfa46bf9bdf4fa34236f2458a6fa0ae025dd
Grzechu <grzegorz.jamrooz@gmail.com>
2021-06-16 21:54:30 +0200

    added SettingFilename

---
+++ src/Domain/SettingFilename.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+namespace PageSourceSystem\Domain;
+
+use SimpleStorageSystem\Utilities\Explorer;
+
+class SettingFilename extends \Stringable
+{
+    public function __construct(
+        private string $setting,
+        private string $renderDirectory
+    ) {
+    }
+
+    public function __toString(): string
+    {
+        $directory = sprintf('%s/settings', $this->renderDirectory);
+        Explorer::createDirectoryIfNotExists($directory);
+
+        return sprintf('%s/%s.json', $directory, $this->setting);
+    }
+}
