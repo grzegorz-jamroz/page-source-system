cae8534deba290dd0338f8bddc6dd65ec74f55b7
Grzechu <grzegorz.jamrooz@gmail.com>
2021-06-23 22:03:27 +0200

    added createDefault method to interface

--- src/Setting/AbstractLanguages.php
+++ src/Setting/AbstractLanguages.php
@@ -5,18 +5,65 @@ declare(strict_types=1);
 namespace PageSourceSystem\Setting;
 
 use Ifrost\PageSourceComponents\AbstractSetting;
+use PlainDataTransformer\Transform;
 
 abstract class AbstractLanguages extends AbstractSetting
 {
+    /**
+     * @param array<int, string> $supportedLanguages
+     */
+    public function __construct(
+        protected string $defaultLanguage,
+        protected array $supportedLanguages,
+    ) {
+    }
+
     final public static function getTypename(): string
     {
         return 'Languages';
     }
 
-    abstract public function getDefaultLanguage(): string;
+    public function getDefaultLanguage(): string
+    {
+        return $this->defaultLanguage;
+    }
 
     /**
      * @return array<int, string>
      */
-    abstract public function getSupportedLanguages(): array;
+    public function getSupportedLanguages(): array
+    {
+        return $this->supportedLanguages;
+    }
+
+    /**
+     * @return array<string, mixed>
+     */
+    public function jsonSerialize(): array
+    {
+        return [
+            '__typename' => self::getTypename(),
+            'defaultLanguage' => $this->defaultLanguage,
+            'supportedLanguages' => $this->supportedLanguages,
+        ];
+    }
+
+    /**
+     * @param array<string, mixed> $data
+     */
+    public static function createFromArray(array $data): self
+    {
+        return new static(
+            Transform::toString($data['defaultLanguage'] ??= ''),
+            Transform::toArray($data['supportedLanguages'] ??= []),
+        );
+    }
+
+    public static function createDefault(): self
+    {
+        return new static(
+            'en',
+            ['en'],
+        );
+    }
 }
